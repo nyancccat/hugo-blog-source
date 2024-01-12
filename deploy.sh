bc75fcbc04bb9be2cbcaee60e25afd238b4e51ec
nyancccat <theqee@gmail.com>
2024-01-12 10:54:57 +0800

    :black_nib: update 2024-01-12 Fri 10:54:57

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo -e "Clearing..."
 # 删除资源文件下生成
-rm -v ./resources/*
+rm -r resources/*
 # 删除 public 文件夹
-rm -v ./public
+rm -r public/
 echo -e "Deploying..."
 git add .
 git status
@@ -11,4 +12,4 @@ if [ -z $msg ];then
   msg=":black_nib: update $(date +'%F %a %T')"
 fi
 git commit -m "$msg"
-git push
+git pushf
--- public/friends/index.md
+++
@@ -1,9 +0,0 @@
-# 友情链接
-
-
-
----
-
-> 作者: [pagezen](http://clearsky.me/)  
-> URL: https://clearsky.me/friends/  
-
